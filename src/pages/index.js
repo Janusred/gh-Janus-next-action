914be3e71a6fca2bc7a08e3a3002ae9f3f2d6aae
Janusred <horaciok300@hotmail.com>
2022-08-20 22:07:21 -0500

    Lint

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <>
       <h1>Hello Next.js</h1>
+      <img src="https://cdn.shopify.com/s/files/1/0267/1597/9810/products/BicicletadeMontanaDobleSuspensionHawkHill1_2021_MarinBikesimportbikemexico_360x.jpg?v=1649178205"></img>
     </>
   );
 }
